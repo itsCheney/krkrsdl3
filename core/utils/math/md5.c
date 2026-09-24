257c09a5552c62b8553d5ce8af0f75e19a67ab47
Mashiro <ohayo@cheney233.moe>
2026-09-24 19:24:23 +0800

    Avoid null pointer arithmetic in MD5 alignment check

--- core/utils/math/md5.c
+++ core/utils/math/md5.c
@@ -58,6 +58,7 @@
 
 
 #include "md5.h"
+#include <stdint.h>
 #include <string.h>
 
 #undef BYTE_ORDER	/* 1 = big-endian, -1 = little-endian, 0 = unknown */
@@ -167,7 +168,7 @@ md5_process(md5_state_t *pms, const md5_byte_t *data /*[64]*/)
 	     * On little-endian machines, we can process properly aligned
 	     * data without copying it.
 	     */
-	    if (!((data - (const md5_byte_t *)0) & 3)) {
+	    if (!((uintptr_t)data & 3)) {
 		/* data are properly aligned */
 		X = (const md5_word_t *)data;
 	    } else {
